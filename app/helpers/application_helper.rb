e4f6b3efcae80b37558199d52475407efd95891a
mishina <temma182008@gmail.com>
2018-10-13 18:48:40 +0900

    ビュー修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def render_notices
     render partial: 'partials/notices'
   end
+
+  def print_link(body, url, alt = 'body is missing')
+    if body.present?
+      link_to body, url
+    else
+      link_to alt, url
+    end
+  end
 end
