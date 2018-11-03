35b2f2769af4fc8744f5a8e0196ede4782074406
mishina <temma182008@gmail.com>
2018-11-03 14:37:54 +0900

    systemテストを修正したかった

--- test/system/channels_test.rb
+++ test/system/channels_test.rb
@@ -1,49 +1,49 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class ChannelsTest < ApplicationSystemTestCase
   setup do
-    @channel = channels(:one)
+    @channel = channels(:チャンネル1)
   end
 
-  test "visiting the index" do
+  test 'visiting the index' do
     visit channels_url
-    assert_selector "h1", text: "Channels"
+    assert_selector 'h1', text: I18n.t('helpers.title.list', models: Channel.model_name.human.pluralize(I18n.locale))
   end
 
-  test "creating a Channel" do
+  test 'creating a Channel' do
     visit channels_url
-    click_on "New Channel"
+    click_on 'New Channel'
 
-    fill_in "Channel", with: @channel.channel_id
-    fill_in "Description", with: @channel.description
-    fill_in "Thumbnail Url", with: @channel.thumbnail_url
-    fill_in "Title", with: @channel.title
-    click_on "Create Channel"
+    fill_in 'Channel', with: @channel.channel_id
+    fill_in 'Description', with: @channel.description
+    fill_in 'Thumbnail Url', with: @channel.thumbnail_url
+    fill_in 'Title', with: @channel.title
+    click_on 'Create Channel'
 
-    assert_text "Channel was successfully created"
-    click_on "Back"
+    assert_text 'Channel was successfully created'
+    click_on 'Back'
   end
 
-  test "updating a Channel" do
+  test 'updating a Channel' do
     visit channels_url
-    click_on "Edit", match: :first
+    click_on 'Edit', match: :first
 
-    fill_in "Channel", with: @channel.channel_id
-    fill_in "Description", with: @channel.description
-    fill_in "Thumbnail Url", with: @channel.thumbnail_url
-    fill_in "Title", with: @channel.title
-    click_on "Update Channel"
+    fill_in 'Channel', with: @channel.channel_id
+    fill_in 'Description', with: @channel.description
+    fill_in 'Thumbnail Url', with: @channel.thumbnail_url
+    fill_in 'Title', with: @channel.title
+    click_on 'Update Channel'
 
-    assert_text "Channel was successfully updated"
-    click_on "Back"
+    assert_text 'Channel was successfully updated'
+    click_on 'Back'
   end
 
-  test "destroying a Channel" do
+  test 'destroying a Channel' do
     visit channels_url
     page.accept_confirm do
-      click_on "Destroy", match: :first
+      click_on 'Destroy', match: :first
     end
 
-    assert_text "Channel was successfully destroyed"
+    assert_text 'Channel was successfully destroyed'
   end
 end
--- test/system/system_settings_test.rb
+++ test/system/system_settings_test.rb
@@ -1,43 +1,43 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class SystemSettingsTest < ApplicationSystemTestCase
   setup do
-    @system_setting = system_settings(:one)
+    @system_setting = system_settings(:システム設定)
   end
 
-  test "visiting the index" do
+  test 'visiting the index' do
     visit system_settings_url
-    assert_selector "h1", text: "System Settings"
+    assert_selector 'h1', text: I18n.t('helpers.title.list', models: Channel.model_name.human.pluralize(I18n.locale))
   end
 
-  test "creating a System setting" do
+  test 'creating a System setting' do
     visit system_settings_url
-    click_on "New System Setting"
+    click_on 'New System Setting'
 
-    fill_in "Api Key", with: @system_setting.api_key
-    click_on "Create System setting"
+    fill_in 'Api Key', with: @system_setting.api_key
+    click_on 'Create System setting'
 
-    assert_text "System setting was successfully created"
-    click_on "Back"
+    assert_text 'System setting was successfully created'
+    click_on 'Back'
   end
 
-  test "updating a System setting" do
+  test 'updating a System setting' do
     visit system_settings_url
-    click_on "Edit", match: :first
+    click_on 'Edit', match: :first
 
-    fill_in "Api Key", with: @system_setting.api_key
-    click_on "Update System setting"
+    fill_in 'Api Key', with: @system_setting.api_key
+    click_on 'Update System setting'
 
-    assert_text "System setting was successfully updated"
-    click_on "Back"
+    assert_text 'System setting was successfully updated'
+    click_on 'Back'
   end
 
-  test "destroying a System setting" do
+  test 'destroying a System setting' do
     visit system_settings_url
     page.accept_confirm do
-      click_on "Destroy", match: :first
+      click_on 'Destroy', match: :first
     end
 
-    assert_text "System setting was successfully destroyed"
+    assert_text 'System setting was successfully destroyed'
   end
 end
