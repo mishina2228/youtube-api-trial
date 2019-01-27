2b9476a483ad736b0d6582070382e21d41f0f75d
mishina <temma182008@gmail.com>
2019-01-27 11:27:56 +0900

    RaspberryPi3B+ では bootsnap が読み込めずにエラーになる

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,9 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+
+# bootsnap fails at Raspberry Pi 3B
+# https://romkey.com/2018/05/14/fixing-rails-5-2-bus-error-on-armv7-raspberry-pi/
+unless File.exist?('/proc/cpuinfo') && File.read('/proc/cpuinfo').include?('ARMv7')
+  require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+end
