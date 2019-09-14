3020a7ccfd0cbba5164c7cb3ed6fd8d020bf5159
mishina <temma182008@gmail.com>
2019-09-14 10:48:24 +0900

    ジョブをスケジュール登録

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -6,6 +6,7 @@ Resque.redis = 'localhost:6379'
 Resque.redis.namespace = "resque:youtube_api_trials:#{Rails.env}"
 Resque.logger = Logger.new('log/resque.log', 5, 10.megabytes)
 Resque.logger.level = Logger::INFO
+Resque.schedule = YAML.load_file(Rails.root.join('config', 'resque_schedule.yml'))
 Resque::Scheduler.dynamic = true
 Resque::Scheduler.configure do |c|
   c.logfile = Rails.root.join('log', 'resque-scheduler.log')
