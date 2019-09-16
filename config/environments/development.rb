ac2d0959fdf92e73b19e328bb0f6c855499c7856
mishina <temma182008@gmail.com>
2019-09-16 11:32:47 +0900

    sassc-railsの設定漏れ

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # sassc-rails turn on inline source maps
+  config.sass.inline_source_maps = true
 end
