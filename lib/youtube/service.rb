b710c04529374dc29e665323bdf2ad33eab2ade6
mishina <temma182008@gmail.com>
2019-09-26 22:19:10 +0900

    list_channels の part にはStringを渡すべきだった

--- lib/youtube/service.rb
+++ lib/youtube/service.rb
@@ -16,13 +16,13 @@ class Youtube::Service
   end
 
   def statistics(channel_id)
-    status, res, error = get_channel(:statistics, channel_id)
+    status, res, error = get_channel('statistics', channel_id)
     statics = res.items.first.try(:statistics) if error.blank?
     ::Youtube::ServiceResponse.new(status, statics, error)
   end
 
   def snippet(channel_id)
-    status, res, error = get_channel(:snippet, channel_id)
+    status, res, error = get_channel('snippet', channel_id)
     snippet = res.items.first.try(:snippet) if error.blank?
     ::Youtube::ServiceResponse.new(status, snippet, error)
   end
