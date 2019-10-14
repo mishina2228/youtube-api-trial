ca5e39a99ecbbcc1e3df3e60f5d32da8c80a305c
mishina <temma182008@gmail.com>
2019-10-14 18:45:11 +0900

    登録チャンネルの総数を表示

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,10 +1,11 @@
 class Subscription
-  attr_accessor :channels, :next_page_token, :prev_page_token
+  attr_accessor :channels, :next_page_token, :prev_page_token, :total_results
 
   # @param response [ListSubscriptionResponse]
   def initialize(response)
     self.next_page_token = response.next_page_token
     self.prev_page_token = response.prev_page_token
+    self.total_results = response.page_info.total_results
     self.channels = response.items.map do |subscription|
       channel_id = subscription.snippet.resource_id.channel_id
       c = Channel.find_or_initialize_by(channel_id: channel_id)
