c3239a9866176c8a3a943efc1d9d8e6027c5417c
mishina <temma182008@gmail.com>
2019-11-13 22:49:45 +0900

    [WIP]チャンネル検索結果から登録する機能

--- app/controllers/channel_lists/search_controller.rb
+++ app/controllers/channel_lists/search_controller.rb
@@ -1,5 +1,7 @@
 class ChannelLists::SearchController < ApplicationController
   def index
+    return unless params[:query]
+
     response = ChannelLists::Search.search(params[:query], token: params[:token])
     @search_results = ChannelLists::Search.new(response, params[:query])
   end
