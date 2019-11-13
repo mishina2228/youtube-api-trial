02daa92c15dca4e70be5107d1230a55a6e321b4c
mishina <temma182008@gmail.com>
2019-11-13 22:29:58 +0900

    チャンネル一覧に検索機能

--- app/assets/javascripts/channel.js
+++ app/assets/javascripts/channel.js
@@ -1,6 +1,7 @@
 $(document).on('turbolinks:load', (function () {
   emojify();
   $('[data-toggle="tooltip"]').tooltip();
+  $('#reset-search').click(reset_search_form);
 }));
 
 function emojify() {
@@ -8,4 +9,10 @@ function emojify() {
     const emojified = emojione.toImage($(this).html());
     $(this).html(emojified);
   });
+}
+
+function reset_search_form() {
+  const $form = $('form.search');
+  const $forms = $form.find('input[type="text"], select');
+  $forms.each((_, elem) => $(elem).val(''))
 }
