d6d39ee51f14998f5ee4e938d14b9c7f8c27b595
mishina <temma182008@gmail.com>
2019-11-13 23:06:17 +0900

    [WIP]チャンネル検索結果から登録する機能 ajax化

--- app/controllers/channel_lists/search_controller.rb
+++ app/controllers/channel_lists/search_controller.rb
@@ -1,8 +1,12 @@
 class ChannelLists::SearchController < ApplicationController
   def index
-    return unless params[:query]
-
-    response = ChannelLists::Search.search(params[:query], token: params[:token])
-    @search_results = ChannelLists::Search.new(response, params[:query])
+    if params[:query].present?
+      response = ChannelLists::Search.search(params[:query], token: params[:token], max_results: 5)
+      @search_results = ChannelLists::Search.new(response, params[:query])
+    end
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
