b6c3a2bbf039da9d390c270afe84b2db3e834afd
mishina <temma182008@gmail.com>
2019-11-16 15:04:19 +0900

    ページ移動中に画面上にスクロールする

--- app/assets/javascripts/channel.js
+++ app/assets/javascripts/channel.js
@@ -2,6 +2,11 @@ $(document).on('turbolinks:load', () => {
   emojify();
   $('[data-toggle="tooltip"]').tooltip();
   $('#reset-search').on('click', reset_search_form);
+  $('#search-result-pagination').on('ajax:beforeSend', _event => {
+    const margin = $('nav.navbar-fixed-top').height();
+    $('html, body').animate({scrollTop: $('form.search').offset().top - margin});
+  }).on('ajax:success', _event => {
+  });
 });
 
 function emojify() {
