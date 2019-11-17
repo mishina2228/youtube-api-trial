cb296d97ca62aaa18e57d256ddafaff9844b94a6
mishina <temma182008@gmail.com>
2019-11-17 21:58:48 +0900

    テストのリファクタ

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,6 +14,16 @@ class ActiveSupport::TestCase
   def admin
     User.find_by!(admin: true)
   end
+
+  def sample_exception
+    exception = Mishina::Youtube::NoChannelError.new('test_channel_id')
+    backtrace = [
+      '..app/jobs/channel/build_statistics_job.rb:22:in `perform`',
+      '..app/models/channel.rb:38:in `build_statistics!`'
+    ]
+    exception.set_backtrace(backtrace)
+    exception
+  end
 end
 
 class ActionDispatch::IntegrationTest
