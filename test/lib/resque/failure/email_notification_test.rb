60c8c843b58f186998f227f1668a3ff933f77f19
mishina <temma182008@gmail.com>
2019-11-22 16:52:26 +0900

    テスト追加

---
+++ test/lib/resque/failure/email_notification_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+module Resque
+  module Failure
+    class EmailNotificationTest < ActiveSupport::TestCase
+      def test_save
+        notification = Resque::Failure::EmailNotification.new(
+          sample_exception, 'worker', 'queue', 'payload'
+        )
+
+        assert User.should_notify.empty?, '通知対象ユーザーがいないことを確認'
+        assert_nil notification.save
+
+        u = users(:admin)
+        assert u.update(email: 'admin@real_domain.com')
+        assert User.should_notify.present?, '通知対象ユーザーがいることを確認'
+        mail = notification.save
+        assert mail.present?, 'メールが送信されること'
+      end
+    end
+  end
+end
