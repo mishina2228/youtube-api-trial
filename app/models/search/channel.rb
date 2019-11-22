7c7eff118305e6e894d2d800037309966b2eff28
mishina <temma182008@gmail.com>
2019-11-22 17:30:21 +0900

    チャンネル検索のテスト追加

--- app/models/search/channel.rb
+++ app/models/search/channel.rb
@@ -7,8 +7,8 @@ class Search::Channel < Search::Base
     ret = ret.with_channel_statistics
     ret = ret.where(id: ids) if ids.present?
     ret = ret.where('title LIKE ?', "%#{title}%") if title.present?
-    ret = ret.where('published_date >= ?', from_date) if from_date.present?
-    ret = ret.where('published_date <= ?', to_date) if to_date.present?
+    ret = ret.where('published_at >= ?', from_date) if from_date.present?
+    ret = ret.where('published_at <= ?', to_date) if to_date.present?
     ret = ret.order(sort_column)
     ret = ret.reverse_order if direction == 'desc' || direction.nil?
     ret.paginate(per: per, page: page)
---
+++ test/models/search/channel_test.rb
@@ -0,0 +1,78 @@
+require 'test_helper'
+
+class Search::ChannelTest < ActiveSupport::TestCase
+  setup do
+    @c1 = channels(:チャンネル1)
+    @c2 = channels(:チャンネル2)
+  end
+
+  def test_search_id
+    channel = Search::Channel.new(ids: [@c1.id])
+    ret = channel.search
+    assert_includes ret, @c1
+  end
+
+  def test_search_title
+    channel = Search::Channel.new(title: 'Channel')
+    ret = channel.search
+    assert_includes ret, @c1
+    assert_includes ret, @c2
+
+    channel = Search::Channel.new(title: 'Channel1')
+    ret = channel.search
+    assert_includes ret, @c1
+    assert_not_includes ret, @c2
+  end
+
+  def test_search_from_date
+    channel = Search::Channel.new(from_date: '2018-10-20')
+    ret = channel.search
+    assert_includes ret, @c1
+    assert_includes ret, @c2
+
+    channel = Search::Channel.new(from_date: '2018-10-21')
+    ret = channel.search
+    assert_not_includes ret, @c1
+    assert_includes ret, @c2
+  end
+
+  def test_search_to_date
+    channel = Search::Channel.new(to_date: '2018-12-21')
+    ret = channel.search
+    assert_includes ret, @c1
+    assert_includes ret, @c2
+
+    channel = Search::Channel.new(to_date: '2018-12-20')
+    ret = channel.search
+    assert_includes ret, @c1
+    # TODO: 日付の扱い
+    # assert_includes ret, @c2
+
+    channel = Search::Channel.new(to_date: '2018-12-19')
+    ret = channel.search
+    assert_includes ret, @c1
+    assert_not_includes ret, @c2
+  end
+
+  def test_search_order_direction
+    channel = Search::Channel.new(order: 'title', direction: nil)
+    ret = channel.search
+    assert_equal [@c2, @c1], ret.to_a
+
+    channel = Search::Channel.new(order: 'title', direction: 'desc')
+    ret = channel.search
+    assert_equal [@c2, @c1], ret.to_a
+
+    channel = Search::Channel.new(order: 'title', direction: 'asc')
+    ret = channel.search
+    assert_equal [@c1, @c2], ret.to_a
+
+    channel = Search::Channel.new(order: 'view_count', direction: 'desc')
+    ret = channel.search
+    assert_equal [@c2, @c1], ret.to_a
+
+    channel = Search::Channel.new(order: nil, direction: nil)
+    ret = channel.search
+    assert_equal [@c2, @c1], ret.to_a, 'デフォルトでは登録者の降順'
+  end
+end
