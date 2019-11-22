23b2e71ff82de6aea413ddb9e6c1a3f618a10793
mishina <temma182008@gmail.com>
2019-11-22 16:16:47 +0900

    Resque管理画面のルーティングテスト

---
+++ test/controllers/resque_controller_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class ResqueControllerTest < ActionDispatch::IntegrationTest
+  test 'should get /resque' do
+    sign_in admin
+
+    get '/resque'
+    assert_response :redirect
+    uri = URI.parse(response.location)
+    assert_equal '/resque/overview', uri.path
+  end
+
+  test 'should not get /resque if logged in as an user' do
+    sign_in user
+
+    assert_raise ActionController::RoutingError do
+      get '/resque'
+    end
+  end
+
+  test 'should not get /resque unless logged in' do
+    assert_raise ActionController::RoutingError do
+      get '/resque'
+    end
+  end
+end
