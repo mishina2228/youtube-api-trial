fc8c7b0276528774a0297049abd36a36c1c64d34
mishina <temma182008@gmail.com>
2019-11-23 23:48:35 +0900

    Ruby 2.6からは Raspberry pi 3B+ でも bootsnap が使える

--- config/boot.rb
+++ config/boot.rb
@@ -1,9 +1,6 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-
-# bootsnap fails at Raspberry Pi 3B
-# https://romkey.com/2018/05/14/fixing-rails-5-2-bus-error-on-armv7-raspberry-pi/
-unless File.exist?('/proc/cpuinfo') && File.read('/proc/cpuinfo').include?('ARMv7')
-  require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
-end
+# bootsnap fails at Raspberry Pi 3B in particular Ruby versions.
+# If you use Ruby version less than 2.6, comment out the line below.
+require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
