600e5d161e9c702b047bc28db17d2b1c89e554bd
mishina <temma182008@gmail.com>
2019-11-24 12:11:43 +0900

    using headless chrome in system test

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,10 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  caps = Selenium::WebDriver::Remote::Capabilities.chrome(
+    'chromeOptions' => {
+      'args' => %w(--headless --disable-gpu --no-sandbox)
+    }
+  )
+  driven_by :selenium, using: :chrome, options: {desired_capabilities: caps}
 end
