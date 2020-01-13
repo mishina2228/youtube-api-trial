e3328d4180540283ed991591a71ec3b297f9bee1
mishina <temma182008@gmail.com>
2020-01-13 22:44:47 +0900

    redis is not required on the test environment

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,15 +1,17 @@
-require 'resque'
-require 'resque-scheduler'
-require 'resque/scheduler/server'
-require 'resque/failure/multiple'
-require 'resque/failure/redis'
-require 'resque/failure/email_notification'
+unless Rails.env.test?
+  require 'resque'
+  require 'resque-scheduler'
+  require 'resque/scheduler/server'
+  require 'resque/failure/multiple'
+  require 'resque/failure/redis'
+  require 'resque/failure/email_notification'
 
-Resque.redis = 'localhost:6379'
-Resque.redis.namespace = "resque:youtube_api_trials:#{Rails.env}"
-Resque.schedule = YAML.load_file(Rails.root.join('config', 'resque_schedule.yml'))
-Resque::Scheduler.dynamic = true
+  Resque.redis = 'localhost:6379'
+  Resque.redis.namespace = "resque:youtube_api_trials:#{Rails.env}"
+  Resque.schedule = YAML.load_file(Rails.root.join('config', 'resque_schedule.yml'))
+  Resque::Scheduler.dynamic = true
 
-Resque::Failure::Multiple.configure do |multi|
-  multi.classes = [Resque::Failure::Redis, Resque::Failure::EmailNotification]
+  Resque::Failure::Multiple.configure do |multi|
+    multi.classes = [Resque::Failure::Redis, Resque::Failure::EmailNotification]
+  end
 end
