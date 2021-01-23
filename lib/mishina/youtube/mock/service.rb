c452fd1243ca6f92363ea6acfa42f1b6e96fa976
mishina <temma182008@gmail.com>
2021-01-23 13:59:51 +0900

    Extract long processes into methods.

--- lib/mishina/youtube/mock/service.rb
+++ lib/mishina/youtube/mock/service.rb
@@ -8,68 +8,24 @@ class Mishina::Youtube::Mock::Service
   def statistics(channel_id)
     case channel_id
     when /error/
-      Mishina::Youtube::ServiceResponse.new(
-        Consts::Statuses::ERROR,
-        nil,
-        Google::Apis::ClientError.new('Invalid request')
-      )
+      client_error_response
     when /blank/
-      Mishina::Youtube::ServiceResponse.new(
-        Consts::Statuses::BLANK,
-        nil,
-        Mishina::Youtube::NoChannelError.new(channel_id)
-      )
+      no_channel_response(channel_id)
     when /hidden_subscriber/
-      Mishina::Youtube::ServiceResponse.new(
-        Consts::Statuses::OK,
-        Google::Apis::YoutubeV3::ChannelStatistics.new(
-          hidden_subscriber_count: true,
-          video_count: 50,
-          view_count: 5000
-        ),
-        nil
-      )
+      hidden_subscriber_channel_statistics_response
     else
-      Mishina::Youtube::ServiceResponse.new(
-        Consts::Statuses::OK,
-        Google::Apis::YoutubeV3::ChannelStatistics.new(
-          hidden_subscriber_count: false,
-          subscriber_count: 100_000,
-          video_count: 50,
-          view_count: 5000
-        ),
-        nil
-      )
+      channel_statistics_response
     end
   end
 
   def snippet(channel_id)
     case channel_id
     when /error/
-      Mishina::Youtube::ServiceResponse.new(
-        Consts::Statuses::ERROR,
-        nil,
-        Google::Apis::ClientError.new('Invalid request')
-      )
+      client_error_response
     when /blank/
-      Mishina::Youtube::ServiceResponse.new(
-        Consts::Statuses::BLANK,
-        nil,
-        Mishina::Youtube::NoChannelError.new(channel_id)
-      )
+      no_channel_response(channel_id)
     else
-      Mishina::Youtube::ServiceResponse.new(
-        Consts::Statuses::OK,
-        Google::Apis::YoutubeV3::ChannelSnippet.new(
-          title: 'dummy channel',
-          description: 'dummy description',
-          thumbnails: Google::Apis::YoutubeV3::ThumbnailDetails.new(
-            default: Google::Apis::YoutubeV3::Thumbnail.new(url: 'https://example.com/thumbnail/dummy')
-          ),
-          published_at: Time.current
-        ),
-        nil
-      )
+      channel_snippet_response
     end
   end
 
@@ -108,6 +64,64 @@ class Mishina::Youtube::Mock::Service
     Google::Apis::YoutubeV3::SearchListsResponse.new(
       items: Array.new(max_results, search_result),
       page_info: Google::Apis::YoutubeV3::PageInfo.new(total_results: max_results)
+    )
+  end
+
+  private
+
+  def client_error_response
+    Mishina::Youtube::ServiceResponse.new(
+      Consts::Statuses::ERROR,
+      nil,
+      Google::Apis::ClientError.new('Invalid request')
+    )
+  end
+
+  def no_channel_response(channel_id)
+    Mishina::Youtube::ServiceResponse.new(
+      Consts::Statuses::BLANK,
+      nil,
+      Mishina::Youtube::NoChannelError.new(channel_id)
+    )
+  end
+
+  def hidden_subscriber_channel_statistics_response
+    Mishina::Youtube::ServiceResponse.new(
+      Consts::Statuses::OK,
+      Google::Apis::YoutubeV3::ChannelStatistics.new(
+        hidden_subscriber_count: true,
+        video_count: 50,
+        view_count: 5000
+      ),
+      nil
+    )
+  end
+
+  def channel_statistics_response
+    Mishina::Youtube::ServiceResponse.new(
+      Consts::Statuses::OK,
+      Google::Apis::YoutubeV3::ChannelStatistics.new(
+        hidden_subscriber_count: false,
+        subscriber_count: 100_000,
+        video_count: 50,
+        view_count: 5000
+      ),
+      nil
+    )
+  end
+
+  def channel_snippet_response
+    Mishina::Youtube::ServiceResponse.new(
+      Consts::Statuses::OK,
+      Google::Apis::YoutubeV3::ChannelSnippet.new(
+        title: 'dummy channel',
+        description: 'dummy description',
+        thumbnails: Google::Apis::YoutubeV3::ThumbnailDetails.new(
+          default: Google::Apis::YoutubeV3::Thumbnail.new(url: 'https://example.com/thumbnail/dummy')
+        ),
+        published_at: Time.current
+      ),
+      nil
     )
   end
 end
