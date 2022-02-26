36670990ffb231f59b427fe22065694429b80f2b
mishina <temma182008@gmail.com>
2022-02-26 15:49:28 +0900

    feat: Enable rack-mini-profiler with Turbo page transitions

---
+++ config/initializers/rack_profiler.rb
@@ -0,0 +1,8 @@
+if Rails.env.development?
+  require 'rack-mini-profiler'
+
+  # Enable support for Hotwire TurboDrive page transitions
+  Rack::MiniProfiler.config.enable_hotwire_turbo_drive_support = true
+  # initialization is skipped so trigger it
+  Rack::MiniProfilerRails.initialize!(Rails.application)
+end
