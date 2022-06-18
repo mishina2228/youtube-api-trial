a49d73fa0e4fc990a4c2eee3586beadf5301dc46
mishina <temma182008@gmail.com>
2022-06-18 14:38:12 +0900

    fix: Permit Symbol object in `mail.yml`
    `action_mailer.deliver_method` should be a symbol object.
    If a string object specified, the following error raised:
    ```
    Connection refused - connect(2) for 127.0.0.1:25 (Errno::ECONNREFUSED)
    ```

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -2,7 +2,9 @@
 
 yml_path = Rails.root.join('config/mail.yml')
 if File.exist?(yml_path)
-  options = YAML.safe_load_file(yml_path, aliases: true, symbolize_names: true)[Rails.env.to_sym]
+  options = YAML.safe_load_file(
+    yml_path, aliases: true, symbolize_names: true, permitted_classes: [Symbol]
+  )[Rails.env.to_sym]
   if options
     Rails.application.config.action_mailer.merge!(
       options.slice(:delivery_method, :smtp_settings)
