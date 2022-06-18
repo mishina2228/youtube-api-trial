a8ebecd37316377ef9fc9f26d61d2e24460e5cad
mishina <temma182008@gmail.com>
2022-06-18 14:26:44 +0900

    refactor: Enable symbolize_names option at `YAML.safe_load_file`
    Now `deep_symbolize_keys` can be omitted

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -2,10 +2,10 @@
 
 yml_path = Rails.root.join('config/mail.yml')
 if File.exist?(yml_path)
-  options = YAML.safe_load_file(yml_path, aliases: true)[Rails.env]
+  options = YAML.safe_load_file(yml_path, aliases: true, symbolize_names: true)[Rails.env.to_sym]
   if options
     Rails.application.config.action_mailer.merge!(
-      options.deep_symbolize_keys.slice(:delivery_method, :smtp_settings)
+      options.slice(:delivery_method, :smtp_settings)
     )
   end
 end
