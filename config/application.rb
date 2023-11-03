5af5b02b9d5b9236f19e6081080ccc1506c45883
mishina <temma182008@gmail.com>
2023-11-03 17:20:07 +0900

    chore: bin/rails app:update
    Upgrading Rails from 7.0.8 to 7.1.1

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,11 @@ module YoutubeApiTrial
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -26,8 +31,6 @@ module YoutubeApiTrial
 
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-
-    config.paths.add 'lib', eager_load: true
 
     config.active_record.sqlite3_production_warning = false
 
