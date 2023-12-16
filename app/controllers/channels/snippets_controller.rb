cc09935479ad0ea4f63f45b3c3fbe181aab5553f
mishina <temma182008@gmail.com>
2023-12-16 09:51:08 +0900

    feat: Redirect to channel_path if not turbo frame request

--- app/controllers/channels/snippets_controller.rb
+++ app/controllers/channels/snippets_controller.rb
@@ -5,6 +5,8 @@ module Channels
     before_action :set_channel
 
     def index
+      return redirect_to channel_path(@channel) unless turbo_frame_request?
+
       channel_snippets = @channel.channel_snippets.paginate(page: params[:page], per: 5).padding(1)
       render partial: 'channels/snippets_frame', locals: {channel_snippets: channel_snippets}
     end
---
+++ test/controllers/channels/snippets_controller_test.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module Channels
+  class SnippetsControllerTest < ActionDispatch::IntegrationTest
+    setup do
+      @channel = channels(:channel1)
+    end
+
+    test 'should get index' do
+      get channel_snippets_path(channel_id: @channel.id), headers: {'Turbo-Frame' => 'foo'}
+      assert_response :success
+    end
+
+    test 'should redirect to channel_path if not turbo frame' do
+      get channel_snippets_path(channel_id: @channel.id)
+      assert_response :redirect
+      assert_redirected_to channel_path(@channel)
+    end
+  end
+end
