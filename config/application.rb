efd70896fba4f332f98c8625843db42efbce72a1
mishina <temma182008@gmail.com>
2024-08-27 22:18:09 +0900

    chore: bin/rails app:update
    Upgrading Rails from 7.1.3.4 to 7.2.1

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,8 @@ require 'rails'
 require 'active_model/railtie'
 require 'active_record/railtie'
 require 'action_controller/railtie'
-require 'action_view/railtie'
 require 'action_mailer/railtie'
+require 'action_view/railtie'
 require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
