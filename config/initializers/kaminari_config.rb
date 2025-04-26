18e06eca676754a38792b2b844853439eda196ff
mishina <temma182008@gmail.com>
2025-04-26 09:09:44 +0900

    update: Set the inner window of the pagination to 5

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -3,7 +3,7 @@
 Kaminari.configure do |config|
   config.default_per_page = 15
   # config.max_per_page = nil
-  config.window = 2
+  config.window = 5
   # config.outer_window = 0
   # config.left = 0
   # config.right = 0
